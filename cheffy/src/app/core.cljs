6c6104137120ed5153425d3fa468d65ad7f0e700
Taras S <tarassuhovenko@gmail.com>
2019-12-16 00:12:40 +0200

    setup lesson

--- cheffy/src/app/core.cljs
+++ cheffy/src/app/core.cljs
@@ -1,15 +1,25 @@
 (ns app.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [re-frame.core :as rf]
+            [app.db]
+            [app.theme :refer [cheffy-theme]]
+            ["@smooth-ui/core-sc" :refer [ThemeProvider Normalize Button]]))
 
 (defn app
   []
-  [:div "Cheffy"])
+  [:<>
+   [:> Normalize]
+   [:> ThemeProvider {:theme cheffy-theme}
+    [:> Button "hello!"]]])
 
 (defn ^:dev/after-load start
   []
+  (rf/dispatch [:initialize-db])
   (r/render [app]
-    (.getElementById js/document "app")))
+            (.getElementById js/document "app")))
 
 (defn ^:export init
   []
   (start))
+
+(js/console.log {:test 2})
