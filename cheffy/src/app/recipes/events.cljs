21204223856eff25071dd7634d98195050a6580c
Taras S <tarassuhovenko@gmail.com>
2020-01-08 17:23:11 +0200

    ingredients3

--- cheffy/src/app/recipes/events.cljs
+++ cheffy/src/app/recipes/events.cljs
@@ -1,5 +1,5 @@
 (ns app.recipes.events
-  (:require [re-frame.core :refer [reg-event-db]]))
+  (:require [re-frame.core :refer [reg-event-db reg-event-fx]]))
 
 (reg-event-db
   :save-recipe
@@ -8,3 +8,24 @@
       (-> db
           (update-in [:users uid :saved] conj recipe-id)
           (update-in [:recipes recipe-id :saved-count] inc)))))
+
+(reg-event-fx
+  :delete-ingredient
+  (fn [{:keys [db]} [_ ingredient-id]]
+    (let [recipe-id (get-in db [:nav :active-recipe])]
+      {:db       (update-in db [:recipes recipe-id :ingredients] dissoc ingredient-id)
+       :dispatch [:close-modal]})))
+
+(reg-event-fx
+  :upsert-ingredient
+  (fn [{:keys [db]} [_ {:keys [id name amount measure]}]]
+    (let [recipe-id   (get-in db [:nav :active-recipe])
+          ingredients (get-in db [:recipes recipe-id :ingredients])
+          order       (or (get-in ingredients [id :order])
+                          (inc (count ingredients)))]
+      {:db       (assoc-in db [:recipes recipe-id :ingredients id] {:id      id
+                                                                    :order   order
+                                                                    :name    name
+                                                                    :amount  amount
+                                                                    :measure measure})
+       :dispatch [:close-modal]})))
--- cheffy/src/app/recipes/views/recipe_ingredients.cljs
+++ cheffy/src/app/recipes/views/recipe_ingredients.cljs
@@ -9,7 +9,7 @@
 
 (defn recipe-ingredients
   []
-  (let [initial-values {:id nil :amount 0 :measure "" :name ""}
+  (let [initial-values {:id nil :amount "" :measure "" :name ""}
         values         (r/atom initial-values)
         open-modal     (fn [{:keys [modal-name ingredient]}]
                          (rf/dispatch [:open-modal modal-name])
@@ -19,63 +19,65 @@
                                                            :name    (str/trim name)
                                                            :amount  (js/parseInt amount)
                                                            :measure (str/trim measure)}])
-                         (reset! values initial-values))
-        ingredients    @(rf/subscribe [:ingredients])
-        author?        @(rf/subscribe [:author?])]
+                         (reset! values initial-values))]
     (fn []
-      [:> Box {:background-color "white"
-               :border-radius    10
-               :p                2
-               :pt               0}
-       [:> Box {:display         "flex"
-                :justify-content "flex-start"}
-        [:> Box
-         [:> Typography {:variant "h5"
-                         :py      15
-                         :m       0}
-          "Ingredients"]]
-        [:> Box {:my 15
-                 :pl 10}
-         [:> Button {:variant  "light"
-                     :size     "small"
-                     :on-click #(open-modal {:modal-name :ingredient-editor
-                                             :ingredient initial-values})}
-          [:> Plus {:size 12}]]]]
-       [:> Box
-        (for [{:keys [id amount measure name]} ingredients]
-          ^{:key id}
-          [:> Box {:py 10}
-           [:> Row
-            [:> Col {:xs 3} amount " " measure]
-            [:> Col name]]])
-        (when author?
-          [modal {:modal-name :ingredient-editor
-                  :header     "Ingredient"
-                  :body       [:<>
-                               [:> Row
-                                [:> Col
-                                 [form-group {:id     :amount
-                                              :label  "Amount"
-                                              :type   "number"
-                                              :values values}]]
-                                [:> Col
-                                 [form-group {:id     :measure
-                                              :label  "Measure"
+      (let [ingredients @(rf/subscribe [:ingredients])
+            author?     @(rf/subscribe [:author?])]
+        [:> Box {:background-color "white"
+                 :border-radius    10
+                 :p                2
+                 :pt               0}
+         [:> Box {:display         "flex"
+                  :justify-content "flex-start"}
+          [:> Box
+           [:> Typography {:variant "h5"
+                           :py      15
+                           :m       0}
+            "Ingredients"]]
+          [:> Box {:my 15
+                   :pl 10}
+           [:> Button {:variant  "light"
+                       :size     "sm"
+                       :on-click #(open-modal {:modal-name :ingredient-editor
+                                               :ingredient initial-values})}
+            [:> Plus {:size 12}]]]]
+         [:> Box
+          (for [{:keys [id amount measure name] :as ingredient} ingredients]
+            ^{:key id}
+            [:> Box {:py 10}
+             [:> Row (when author? {:class    "editable"
+                                    :on-click #(open-modal {:modal-name :ingredient-editor
+                                                            :ingredient ingredient})})
+              [:> Col {:xs 3} amount " " measure]
+              [:> Col name]]])
+          (when author?
+            [modal {:modal-name :ingredient-editor
+                    :header     "Ingredient"
+                    :body       [:<>
+                                 [:> Row
+                                  [:> Col
+                                   [form-group {:id     :amount
+                                                :label  "Amount"
+                                                :type   "number"
+                                                :values values}]]
+                                  [:> Col
+                                   [form-group {:id     :measure
+                                                :label  "Measure"
+                                                :type   "text"
+                                                :values values}]]]
+                                 [form-group {:id     :name
+                                              :label  "Name"
                                               :type   "text"
-                                              :values values}]]]
-                               [form-group {:id     :name
-                                            :label  "Name"
-                                            :type   "text"
-                                            :values values}]]
-                  :footer     [:<>
-                               (when-let [ingredient-id (:id @values)]
-                                 [:a {:href     "#"
-                                      :on-click #(when (js/confirm "Are you sure?")
-                                                   (rf/dispatch [:delete-ingredient ingredient-id]))}
-                                  "Delete"])
-                               [:> Button {:variant  "light"
-                                           :mx       10
-                                           :on-click #(rf/dispatch [:close-modal])}
-                                "Cancel"]
-                               [:> Button {:on-click #(save @values)}
-                                "Save"]]}])]])))
+                                              :values values}]]
+                    :footer     [:<>
+                                 (when-let [ingredient-id (:id @values)]
+                                   [:a {:href     "#"
+                                        :on-click #(when (js/confirm "Are you sure?")
+                                                     (rf/dispatch [:delete-ingredient ingredient-id]))}
+                                    "Delete"])
+                                 [:> Button {:variant  "light"
+                                             :mx       10
+                                             :on-click #(rf/dispatch [:close-modal])}
+                                  "Cancel"]
+                                 [:> Button {:on-click #(save @values)}
+                                  "Save"]]}])]]))))
