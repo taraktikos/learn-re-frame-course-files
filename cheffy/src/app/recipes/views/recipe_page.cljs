733b3a2773bc41518c1ef306f849794ff9815010
Taras S <tarassuhovenko@gmail.com>
2020-01-08 22:06:08 +0200

    recipe editor

--- cheffy/src/app/recipes/views/recipe_page.cljs
+++ cheffy/src/app/recipes/views/recipe_page.cljs
@@ -5,13 +5,17 @@
             [app.recipes.views.recipe-image :refer [recipe-image]]
             [app.recipes.views.recipe-ingredients :refer [recipe-ingredients]]
             [app.recipes.views.recipe-steps :refer [recipe-steps]]
+            [app.recipes.views.recipe-editor :refer [recipe-editor]]
             ["@smooth-ui/core-sc" :refer [Box Row Col]]))
 
 (defn recipe-page
   []
-  (let [{:keys [name]} @(rf/subscribe [:recipe])]
+  (let [{:keys [name]} @(rf/subscribe [:recipe])
+        author?        @(rf/subscribe [:author?])]
     [:> Box
-     [page-nav {:center name}]
+     [page-nav {:center (if author?
+                          [recipe-editor]
+                          name)}]
      [:> Box
       [:> Row
        [:> Col {:xs 12 :sm 6}
